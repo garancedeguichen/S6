799b830679da2e61250bf645c1098744f7c5349e
garancedeguichen <garancedeguichen@gmail.com>
2024-02-02 12:54:29 +0100

    rg

---
+++ input-ex.py
@@ -0,0 +1,20 @@
+name = input("What is your name? ")
+age = input("How old are you? ")
+# print("Hello, ", name, "!", sep="")
+try:
+    age = int(age) # convert string to integer
+    birth_year = 2023 - age
+    print(name, ", you were born in ", birth_year, ".", sep="")
+    number = input("Give me a number to divide the age: ")
+    number = int(number)
+    print(age / number)
+except ValueError:
+    print("Invalid age. Please enter a number")
+except ZeroDivisionError:
+    print("You cannot divide by zero")
+except:
+    print("Some other error I did not forsee")
+else:
+    print("No exceptions were raised")
+finally:
+    print("Thank you for playing")
